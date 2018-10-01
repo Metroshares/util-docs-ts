ab48a04962d456121b10d0c32b8cfd7628818c91
Sandwich <hi.na3s@gmail.com>
2018-10-01 04:41:31 +0200

    publish gh-pages

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,5 +5,7 @@ PATH_DOCS=${1-"./docs"}
 
 npm install --no-save typedoc@0.12.0 typedoc-plugin-external-module-name typedoc-plugin-markdown
 
+cp $PATH_DOCS/book/theme/redirect.html $PATH_DOCS/index.html
+
 cd $PATH_DOCS/book
 npm install
